838e8494c76e2e12b8888f04a48ad063c776f184
Ben Klein <bklein@pivotal.io>
2019-04-25 14:48:26 -0700

    Redirect docs URLs without version prefix to 1.4

--- redirects.rb
+++ redirects.rb
@@ -3,6 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-3/$1"
+r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-4/$1"
 r301 %r{/scdf/1-0/(.*)}, 'https://docs.pivotal.io/archives/scdf-docs-1.0.pdf'
 r301 %r{/scdf/1-1/(.*)}, 'https://docs.pivotal.io/archives/scdf-docs-1.1.pdf'
