fb40b5ac676ef5506b4f2b2365499e4158b51673
radaeze <ramia.davis@gmail.com>
2020-05-26 10:29:16 -0700

    update redirects/subnav [#173019911]

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-7/$1"
+r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-8/$1"
