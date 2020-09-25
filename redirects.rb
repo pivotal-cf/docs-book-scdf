573cf9e7525200821b6995be65c2b9041b1c5bc8
Ben Klein <bklein@pivotal.io>
2020-09-25 13:01:51 -0500

    Gratuitous comment

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
+# If no version prefix is provided, redirect to docs for latest version
 r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-9/$1"
