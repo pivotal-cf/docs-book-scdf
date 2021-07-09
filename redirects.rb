3437597ca856c3565be2af0a3549d629d4a82075
Ben Klein <bklein@pivotal.io>
2021-07-09 18:41:17 -0500

    Testing... add 'main'/'pro' prefix to all versions

--- redirects.rb
+++ redirects.rb
@@ -4,7 +4,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # If no dir prefix is provided, use 'main'
-r301 %r{/scdf/1-11/(?!main|pro-features)(.*)}, "/scdf/1-11/main/$1"
+r301 %r{/scdf/([0-9]-[0-9]{2})/(?!main|pro-features)(.*)}, "/scdf/$1/main/$2"
 
 # If no version prefix is provided, redirect to docs for latest version
 r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-11/$1"
