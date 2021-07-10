c9bc358ac05fe9a7e7f258bc9738645dc9b1aa96
Ben Klein <bklein@pivotal.io>
2021-07-09 20:10:43 -0500

    Fix links from 1.11 to earlier versions

--- redirects.rb
+++ redirects.rb
@@ -6,5 +6,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # If no dir prefix is provided, use 'main'
 r301 %r{/scdf/1-11/(?!main|pro-features)(.*)}, "/scdf/1-11/main/$1"
 
+# Omit 'main' prefix for versions earlier than 1.11
+r301 %r{/scdf/(1-[0-9]|10)/main(.*)}, "/scdf/$1$3"
+
 # If no version prefix is provided, redirect to docs for latest version
 r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-11/$1"
