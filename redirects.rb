c58bb1ce23faf611585d836c8b74ad15ac018ebf
Ben Klein <bklein@pivotal.io>
2021-07-09 19:14:19 -0500

    Test

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# If no dir prefix is provided, use 'main'
-r301 %r{/scdf/1-11/(?!main|pro-features)(.*)}, "/scdf/1-11/main/$1"
-
 # If no version prefix is provided, redirect to docs for latest version
 r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-11/$1"
