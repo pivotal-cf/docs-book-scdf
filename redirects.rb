15430ea1cadb9ebff0909ecc7728c72141ede26a
Ben Klein <bklein@pivotal.io>
2021-07-09 20:34:29 -0500

    Actually fix the regular expression

--- redirects.rb
+++ redirects.rb
@@ -7,7 +7,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 r301 %r{/scdf/1-11/(?!main|pro-features)(.*)}, "/scdf/1-11/main/$1"
 
 # Omit 'main' prefix for versions earlier than 1.11
-r301 %r{/scdf/(1-[0-9]|10)/main/(.*)}, "/scdf/$1/$3"
+r301 %r{/scdf/(1-[0-9]|10)/main/(.*)}, "/scdf/$1/$2"
 
 # If no version prefix is provided, redirect to docs for latest version
 r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-11/$1"
