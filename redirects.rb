b9c81d66db5ead9ec36d550b6e312e930b850346
Ben Klein <bklein@pivotal.io>
2021-11-04 19:41:58 -0500

    Revert "another test"
    This reverts commit b4e2062153ee23d7a81bc6f6b22aa2bd68e50ae7.

--- redirects.rb
+++ redirects.rb
@@ -5,6 +5,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Versions 1.11 and later: if no dir prefix is provided, use 'main'
 r301 %r{/scdf/1-12/(?!main|pro-features)(.*)}, "/scdf/1-12/main/$1"
+r301 %r{/scdf/1-11/(?!main|pro-features)(.*)}, "/scdf/1-11/main/$1"
 
 # Versions 1.10 and earlier: remove 'main' prefix if provided
 r301 %r{/scdf/(1-[0-9]|10)/main/(.*)}, "/scdf/$1/$2"
